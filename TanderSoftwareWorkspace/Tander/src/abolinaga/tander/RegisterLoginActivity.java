d49dafb9f6fd284becaee346de12a6d4f29443c4
Asier Bolinaga <abolinaga@eucoeng.es>
2015-11-18 08:31:05 +0100

    EA architecture and Eclipse project added

---
+++ TanderSoftwareWorkspace/Tander/src/abolinaga/tander/RegisterLoginActivity.java
@@ -0,0 +1,14 @@
+package abolinaga.tander;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RegisterLoginActivity extends Activity 
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.registerlogin);
+	}
+}
