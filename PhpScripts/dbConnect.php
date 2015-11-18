57878f5481dd5781b354621b728928ad5916feb5
Asier Bolinaga <abolinaga@eucoeng.es>
2015-11-18 10:34:57 +0100

    PhpScripts added

---
+++ PhpScripts/dbConnect.php
@@ -0,0 +1,8 @@
+<?php
+
+	define('HOST', '');
+	define('USERNAME','');
+	define('PASSWORD','');
+	define('DB','');
+	
+	$con = mysqli_connect(HOSTm USERNAME, PASSWORD, DB) or die('Unable to connect');
