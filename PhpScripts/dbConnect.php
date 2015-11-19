8b620f3fdd2dea3bdb2eb635ca59e235499549de
Asier Bolinaga <abolinaga@eucoeng.es>
2015-11-19 07:32:31 +0100

    php files changed for new DB

--- PhpScripts/dbConnect.php
+++ PhpScripts/dbConnect.php
@@ -1,8 +1,8 @@
 <?php
 
-	define('HOST', 'mysql.hostinger.in');
-	define('USERNAME','u640208784_tapp');
+	define('HOST', 'mysql.hostinger.es');
+	define('USERNAME','u987120479_abtan');
 	define('PASSWORD','5vaxequ9');
-	define('DB','u640208784_t0000');
+	define('DB',' u987120479_t0000 ');
 	
 	$con = mysqli_connect(HOST, USERNAME, PASSWORD, DB) or die('Unable to connect');
--- PhpScripts/register.php
+++ PhpScripts/register.php
@@ -1,40 +1,41 @@
 <?php
-if($_SERVER['REQUEST_METHOD']=='POST'){
- $name = $_POST['name']; 
- $username = $_POST['username']; 
- $password = $_POST['password']; 
- $email = $_POST['email']; 
- $languagetoteach = $_POST['languagetoteach']; 
- $languagetolearn = $_POST['languagetolearn'];  
- if($name == '' || $username == '' || $password == '' || $email == '')
- { 
- echo 'please fill all values'; 
- }
- else
- { 
- require_once('dbConnect.php'); 
- $sql = "SELECT * FROM users WHERE username='$username' OR email='$email'";  
- $check = mysqli_fetch_array(mysqli_query($con,$sql));  
- if(isset($check))
- { 
- echo 'username or email already exist'; 
- }
- else
- {  
- $sql = "INSERT INTO users (name,username,password,email) VALUES('$name','$username','$password','$email','$languagetoteach','$languagetolearn')"; 
- if(mysqli_query($con,$sql))
- { 
- echo 'successfully registered'; 
- }
- else
- { 
- echo 'oops! Please try again!'; 
- } 
- } 
- mysqli_close($con); 
- }
- }
- else
- {
- echo 'error';
- }
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+	$name = $_POST['name']; 
+	$username = $_POST['username']; 
+	$password = $_POST['password']; 
+	$email = $_POST['email']; 
+	$languagetoteach = $_POST['languagetoteach']; 
+	$languagetolearn = $_POST['languagetolearn'];  
+	if($name == '' || $username == '' || $password == '' || $email == '' || $languagetoteach == '' || $languagetolearn == '')
+	{ 
+		echo 'please fill all values'; 
+	}
+	else
+	{ 
+		require_once('dbConnect.php'); 
+		$sql = "SELECT * FROM users WHERE username='$username' OR email='$email'";  
+		$check = mysqli_fetch_array(mysqli_query($con,$sql));  
+		if(isset($check))
+		{ 
+			echo 'username or email already exist'; 
+		}
+		else
+		{
+			$sql = "INSERT INTO users (name,username,password,email) VALUES('$name','$username','$password','$email','$languagetoteach','$languagetolearn')"; 
+			if(mysqli_query($con,$sql))
+			{ 
+				echo 'successfully registered'; 
+			}
+			else
+			{ 
+				echo 'oops! Please try again!'; 
+			} 
+		} 
+		mysqli_close($con); 
+	}
+}
+else
+{
+	echo 'error';
+}
